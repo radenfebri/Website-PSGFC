0068b45a11af1f9122c0116a2239930e2b2955b1
febri2001 <febriye12@gmail.com>
2021-10-23 08:49:23 +0700

    Upload Terbaru

--- resources/views/front/blog/blog.blade.php
+++ resources/views/front/blog/blog.blade.php
@@ -10,132 +10,140 @@
 
     <!-- ======= Breadcrumbs ======= -->
     <section id="breadcrumbs" class="breadcrumbs">
-      <div class="container pt-5">
+        <div class="container pt-5">
 
-        <div class="d-flex justify-content-between align-items-center">
-          <h2>Blog</h2>
-          <ol>
-            <li><a href="/">Home</a></li>
-            <li>Blog</li>
-          </ol>
+            <div class="d-flex justify-content-between align-items-center">
+                <h2>Blog</h2>
+                <ol>
+                    <li><a href="/">Home</a></li>
+                    <li>Blog</li>
+                </ol>
+            </div>
+
         </div>
-
-      </div>
     </section><!-- End Breadcrumbs -->
 
     <!-- ======= Blog Section ======= -->
     <br>
-    <div class="row justify-content-center">
+    {{-- <div class="row justify-content-center">
         <div class="col-md-6">
             <form class="d-flex" action="/blog">
                 <input class="form-control me-2" name="search" type="search" value="{{ request('search') }}" placeholder="Search" aria-label="Search">
                 <button class="btn btn-outline-primary" type="submit">Search</button>
             </form>
         </div>
-    </div>
+    </div> --}}
 
     <section id="blog" class="blog">
-      <div class="container" data-aos="fade-up">
-        <div class="row">
-          <div class="col-lg-8 entries">
+        <div class="container" data-aos="fade-up">
+            <div class="row">
+                <div class="col-lg-8 entries">
 
-            @foreach ($blog as $item)
-            <article class="entry">
-              <div class="entry-img ">
-                <img src="{{ asset('storage/'. $item->gambar_artikel) }}" alt="" class="img-fluid">
-              </div>
+                    @foreach ($blog as $item)
+                    <article class="entry">
+                        <div class="entry-img ">
+                            <img src="{{ asset('storage/'. $item->gambar_artikel) }}" alt="" class="img-fluid">
+                        </div>
 
-              <h2 class="entry-title">
-                <a href="{{ route('detail', $item->slug ) }}">{{ $item->judul }}</a>
-              </h2>
+                        <h2 class="entry-title">
+                            <a href="{{ route('detail', $item->slug ) }}">{{ $item->judul }}</a>
+                        </h2>
 
-              <div class="entry-meta">
-                <ul>
-                  <li class="d-flex align-items-center"><i class="bi bi-person"></i> <a href="{{ route('detail', $item->slug ) }}">{{ $item->users->name }}</a></li>
-                  <li class="d-flex align-items-center"><i class="bi bi-clock"></i> <a href="{{ route('detail', $item->slug ) }}">{{date('d M Y',strtotime($item->created_at))}}</a></li>
-                  <li class="d-flex align-items-center"><i class="bi bi-chat-dots"></i> <a href="{{ route('detail', $item->slug ) }}">Coment</a></li>
+                        <div class="entry-meta">
+                            <ul>
+                                <li class="d-flex align-items-center"><i class="bi bi-person"></i> <a href="{{ route('detail', $item->slug ) }}">{{ $item->users->name }}</a></li>
+                                <li class="d-flex align-items-center"><i class="bi bi-clock"></i> <a href="{{ route('detail', $item->slug ) }}">{{date('d M Y',strtotime($item->created_at))}}</a></li>
+                                <li class="d-flex align-items-center"><i class="bi bi-chat-dots"></i> <a href="{{ route('detail', $item->slug ) }}">Coment</a></li>
 
-                </ul>
-              </div>
+                            </ul>
+                        </div>
 
-              <div class="entry-content">
-                <p>
-                {!! \Illuminate\Support\Str::words($item->body, 50,'....') !!}
+                        <div class="entry-content">
+                            <p>
+                                {!! \Illuminate\Support\Str::words($item->body, 50,'....') !!}
 
-                {{-- {!! $item->body !!} --}}
-                </p>
-                <div class="read-more">
-                  <a href="{{ route('detail', $item->slug ) }}">Read More</a>
-                </div>
-              </div>
-            </article>
+                                {{-- {!! $item->body !!} --}}
+                            </p>
+                            <div class="read-more">
+                                <a href="{{ route('detail', $item->slug ) }}">Read More</a>
+                            </div>
+                        </div>
+                    </article>
 
-            @endforeach
-
-
-            <div class="blog-pagination">
-                <ul class="justify-content-center">
-                    {{ $blog->links() }}
-                </ul>
-            </div>
+                    @endforeach
 
 
-            <!-- End blog entry -->
-
-
-            {{-- <div class="blog-pagination">
-              <ul class="justify-content-center">
-                <li>{{ $blog->links() }}</li>
-              </ul>
-            </div> --}}
-
-          </div>
-
-
-          <div class="col-lg-4">
-
-            <div class="sidebar">
-
-            @forelse ($iklan as $item)
-            <h3 class="sidebar-title">{{ $item->judul }}</h3>
-            <div class="sidebar-item">
-              <div class="post-item clearfix">
-                <a href="{{ $item->link }}" class="">
-                  <img src="{{ asset('storage/'. $item->gambar_iklan) }}" alt="" class="img-fluid">
-                </a>
-              </div>
-            </div>
-            @empty
-            <p>Iklan Masih Kosong</p>
-            @endforelse ()
-
-
-
-
-              <h3 class="sidebar-title">Recent Posts</h3>
-              <div class="sidebar-item recent-posts">
-                @forelse ($postinganTerbaru as $item)
-                    <div class="post-item clearfix">
-                    <img src="{{ asset('storage/'. $item->gambar_artikel) }}" alt="">
-                    <h4><a href="{{ route('detail', $item->slug ) }}">{{ $item->judul }}</a></h4>
-                    <time datetime="2020-01-01">{{date('d M Y ',strtotime($item->created_at))}}</time>
+                    <div class="blog-pagination">
+                        <ul class="justify-content-center">
+                            {{ $blog->links() }}
+                        </ul>
                     </div>
-                @empty
-                <p>Data Masih Kosong</p>
-                @endforelse
-
-              </div><!-- End sidebar recent posts-->
-
-            </div><!-- End sidebar -->
-
-          </div><!-- End blog sidebar -->
-        </div>
-
-    </section><!-- End Blog Section -->
-
-  </main><!-- End #main -->
-
-  <script id="dsq-count-scr" src="//psg-fc.disqus.com/count.js" async></script>
 
 
-@endsection
+                    <!-- End blog entry -->
+
+
+                    {{-- <div class="blog-pagination">
+                        <ul class="justify-content-center">
+                            <li>{{ $blog->links() }}</li>
+                        </ul>
+                    </div> --}}
+
+                </div>
+
+
+                <div class="col-lg-4">
+
+                    <div class="sidebar">
+                        <h3 class="sidebar-title">Search</h3>
+                        <div class="sidebar-item search-form">
+                            <form action="{{ route('blog') }}">
+                                <input type="text" name="search" id="search" value="{{ request('search') }}">
+                                <button type="submit"><i class="bi bi-search"></i></button>
+                            </form>
+                        </div>
+                        <!-- End sidebar search formn-->
+
+                        @forelse ($iklan as $item)
+                        <h3 class="sidebar-title">{{ $item->judul }}</h3>
+                        <div class="sidebar-item">
+                            <div class="post-item clearfix">
+                                <a href="{{ $item->link }}" class="">
+                                    <img src="{{ asset('storage/'. $item->gambar_iklan) }}" alt="" class="img-fluid">
+                                </a>
+                            </div>
+                        </div>
+                        @empty
+                        <p>Iklan Masih Kosong</p>
+                        @endforelse ()
+
+
+
+
+                        <h3 class="sidebar-title">Recent Posts</h3>
+                        <div class="sidebar-item recent-posts">
+                            @forelse ($postinganTerbaru as $item)
+                            <div class="post-item clearfix">
+                                <img src="{{ asset('storage/'. $item->gambar_artikel) }}" alt="">
+                                <h4><a href="{{ route('detail', $item->slug ) }}">{{ $item->judul }}</a></h4>
+                                <time datetime="2020-01-01">{{date('d M Y ',strtotime($item->created_at))}}</time>
+                            </div>
+                            @empty
+                            <p>Data Masih Kosong</p>
+                            @endforelse
+
+                        </div><!-- End sidebar recent posts-->
+
+                    </div><!-- End sidebar -->
+
+                </div><!-- End blog sidebar -->
+            </div>
+
+        </section><!-- End Blog Section -->
+
+    </main><!-- End #main -->
+
+    <script id="dsq-count-scr" src="//psg-fc.disqus.com/count.js" async></script>
+
+
+    @endsection
--- resources/views/front/blog/detail.blade.php
+++ resources/views/front/blog/detail.blade.php
@@ -10,129 +10,137 @@
 
     <!-- ======= Breadcrumbs ======= -->
     <section id="breadcrumbs" class="breadcrumbs">
-      <div class="container pt-5">
+        <div class="container pt-5">
 
-        <div class="d-flex justify-content-between align-items-center">
-          <h2>Single Post</h2>
-          <ol>
-            <li><a href="/">Home</a></li>
-            <li>Blog</li>
-          </ol>
+            <div class="d-flex justify-content-between align-items-center">
+                <h2>Single Post</h2>
+                <ol>
+                    <li><a href="/">Home</a></li>
+                    <li>Blog</li>
+                </ol>
+            </div>
+
         </div>
-
-      </div>
     </section><!-- End Breadcrumbs -->
 
     <!-- ======= Blog Section ======= -->
     <section id="blog" class="blog">
-      <div class="container" data-aos="fade-up">
+        <div class="container" data-aos="fade-up">
 
-        <div class="row">
-        {{-- @foreach ($blog as $item) --}}
-          <div class="col-lg-8 entries">
+            <div class="row">
+                {{-- @foreach ($blog as $item) --}}
+                <div class="col-lg-8 entries">
 
-            <article class="entry">
+                    <article class="entry">
 
-              <div class="entry-img">
-                <img src="{{ asset('storage/'. $blog->gambar_artikel) }}" alt="" class="img-fluid">
-              </div>
+                        <div class="entry-img">
+                            <img src="{{ asset('storage/'. $blog->gambar_artikel) }}" alt="" class="img-fluid">
+                        </div>
 
-              <h2 class="entry-title">
-                <a href="{{ route('detail', $blog->slug ) }}">{{ $blog->judul }}</a>
-              </h2>
+                        <h2 class="entry-title">
+                            <a href="{{ route('detail', $blog->slug ) }}">{{ $blog->judul }}</a>
+                        </h2>
 
-              <div class="entry-meta">
-                <ul>
-                  <li class="d-flex align-items-center"><i class="bi bi-person"></i> <a href="{{ route('detail', $blog->slug ) }}">{{ $blog->users->name }}</a></li>
-                  <li class="d-flex align-items-center"><i class="bi bi-clock"></i> <a href="{{ route('detail', $blog->slug ) }}">{{date('d M Y',strtotime($blog->created_at))}}</a></li>
-                  {{-- <li class="d-flex align-items-center"><i class="bi bi-chat-dots"></i> <a href="#">12 Comments</a></li> --}}
-                </ul>
-              </div>
-              <!-- ShareThis BEGIN --><div class="sharethis-inline-share-buttons"></div><!-- ShareThis END -->
+                        <div class="entry-meta">
+                            <ul>
+                                <li class="d-flex align-items-center"><i class="bi bi-person"></i> <a href="{{ route('detail', $blog->slug ) }}">{{ $blog->users->name }}</a></li>
+                                <li class="d-flex align-items-center"><i class="bi bi-clock"></i> <a href="{{ route('detail', $blog->slug ) }}">{{date('d M Y',strtotime($blog->created_at))}}</a></li>
+                                {{-- <li class="d-flex align-items-center"><i class="bi bi-chat-dots"></i> <a href="#">12 Comments</a></li> --}}
+                            </ul>
+                        </div>
+                        <!-- ShareThis BEGIN --><div class="sharethis-inline-share-buttons"></div><!-- ShareThis END -->
 
-              <div class="entry-content">
-                <p>
-                {{-- {!! \Illuminate\Support\Str::words($item->body, 50,'....') !!} --}}
+                        <div class="entry-content">
+                            <p>
+                                {{-- {!! \Illuminate\Support\Str::words($item->body, 50,'....') !!} --}}
 
-                {!! $blog->body !!}
-                </p>
-              </div>
+                                {!! $blog->body !!}
+                            </p>
+                        </div>
 
-            </article>
-
-
-            {{-- @endforeach --}}
-
-            <!-- End blog entry -->
+                    </article>
 
 
-            {{-- <div class="blog-pagination">
-              <ul class="justify-content-center">
-                <li><a href="#" class="active">1</a></li>
-                <li class=""><a href="#">2</a></li>
-                <li><a href="#">3</a></li>
-              </ul>
-            </div> --}}
-          </div>
+                    {{-- @endforeach --}}
+
+                    <!-- End blog entry -->
 
 
-          <div class="col-lg-4">
+                    {{-- <div class="blog-pagination">
+                        <ul class="justify-content-center">
+                            <li><a href="#" class="active">1</a></li>
+                            <li class=""><a href="#">2</a></li>
+                            <li><a href="#">3</a></li>
+                        </ul>
+                    </div> --}}
+                </div>
 
-            <div class="sidebar">
 
-            @forelse ($iklan as $item)
-            <h3 class="sidebar-title">Iklan</h3>
-            <div class="sidebar-item">
-              <div class="post-item clearfix">
-                <a href="{{ $item->link }}" class="">
-                  <img src="{{ asset('storage/'. $item->gambar_iklan) }}" alt="" class="img-fluid">
-                </a>
-              </div>
+                <div class="col-lg-4">
+
+                    <div class="sidebar">
+                        <h3 class="sidebar-title">Search</h3>
+                        <div class="sidebar-item search-form">
+                            <form action="{{ route('blog') }}">
+                                <input type="text" name="search" id="search" value="{{ request('search') }}">
+                                <button type="submit"><i class="bi bi-search"></i></button>
+                            </form>
+                        </div>
+                        <!-- End sidebar search formn-->
+
+                        @forelse ($iklan as $item)
+                        <h3 class="sidebar-title">Iklan</h3>
+                        <div class="sidebar-item">
+                            <div class="post-item clearfix">
+                                <a href="{{ $item->link }}" class="">
+                                    <img src="{{ asset('storage/'. $item->gambar_iklan) }}" alt="" class="img-fluid">
+                                </a>
+                            </div>
+                        </div>
+                        @empty
+                        <p>Iklan Masih Kosong</p>
+                        @endforelse ()
+
+
+
+
+                        <h3 class="sidebar-title">Recent Posts</h3>
+                        <div class="sidebar-item recent-posts">
+                            @forelse ($postinganTerbaru as $item)
+                            <div class="post-item clearfix">
+                                <img src="{{ asset('storage/'. $item->gambar_artikel) }}" alt="">
+                                <h4><a href="{{ route('detail', $item->slug ) }}">{{ $item->judul }}</a></h4>
+                                <time datetime="2020-01-01">{{date('d M Y ',strtotime($item->created_at))}}</time>
+                            </div>
+                            @empty
+                            <p>Data Masih Kosong</p>
+                            @endforelse
+
+                        </div><!-- End sidebar recent posts-->
+
+                    </div><!-- End sidebar -->
+
+                </div><!-- End blog sidebar -->
             </div>
-            @empty
-            <p>Iklan Masih Kosong</p>
-            @endforelse ()
 
+        </section><!-- End Blog Section -->
 
-
-
-            <h3 class="sidebar-title">Recent Posts</h3>
-            <div class="sidebar-item recent-posts">
-              @forelse ($postinganTerbaru as $item)
-                  <div class="post-item clearfix">
-                  <img src="{{ asset('storage/'. $item->gambar_artikel) }}" alt="">
-                  <h4><a href="{{ route('detail', $item->slug ) }}">{{ $item->judul }}</a></h4>
-                  <time datetime="2020-01-01">{{date('d M Y ',strtotime($item->created_at))}}</time>
-                  </div>
-              @empty
-              <p>Data Masih Kosong</p>
-              @endforelse
-
-              </div><!-- End sidebar recent posts-->
-
-            </div><!-- End sidebar -->
-
-          </div><!-- End blog sidebar -->
-        </div>
-
-    </section><!-- End Blog Section -->
-
-<br>
-<br>
-<div class="container" data-aos="fade-up">
-    <div id="disqus_thread" class="mt-4"></div>
-    <script>
-        (function() { // DON'T EDIT BELOW THIS LINE
-        var d = document, s = d.createElement('script');
-        s.src = 'https://psg-fc.disqus.com/embed.js';
-        s.setAttribute('data-timestamp', +new Date());
-        (d.head || d.body).appendChild(s);
-        })();
-    </script>
-  <noscript>Please enable JavaScript to view the <a href="https://disqus.com/?ref_noscript">comments powered by Disqus.</a></noscript>
-  </div>
-<br>
-<br>
+        <br>
+        <br>
+        <div class="container" data-aos="fade-up">
+            <div id="disqus_thread" class="mt-4"></div>
+            <script>
+                (function() { // DON'T EDIT BELOW THIS LINE
+                var d = document, s = d.createElement('script');
+                s.src = 'https://psg-fc.disqus.com/embed.js';
+                s.setAttribute('data-timestamp', +new Date());
+                (d.head || d.body).appendChild(s);
+            })();
+        </script>
+        <noscript>Please enable JavaScript to view the <a href="https://disqus.com/?ref_noscript">comments powered by Disqus.</a></noscript>
+    </div>
+    <br>
+    <br>
 </main><!-- End #main -->
 
 
